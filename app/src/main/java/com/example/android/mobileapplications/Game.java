263ec6f494eb3274ece2c9d0e44a06a928f981de
kinoodaphne <daphne.kinoo@outlook.be>
2018-12-30 21:26:50 +0100

    v1.1
    - Creating layout
    - Creating Main activity
    - Creating Game activity
    - Adding text via EditText
    - Function onClick to start game
    - Function to roll 3 dices at once
    - Adding animation Shake on dices

---
+++ app/src/main/java/com/example/android/mobileapplications/Game.java
@@ -0,0 +1,82 @@
+package com.example.android.mobileapplications;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class Game extends AppCompatActivity {
+
+    private static final Random RANDOM = new Random();
+    Button btn_roll;
+    ImageView iv_dice1, iv_dice2, iv_dice3;
+
+    TextView tv_score;
+
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        btn_roll = findViewById(R.id.bn_roll);
+        iv_dice1 = findViewById(R.id.iv_dice1);
+        iv_dice2 = findViewById(R.id.iv_dice2);
+        iv_dice3 = findViewById(R.id.iv_dice3);
+
+        btn_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Animation anim1 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
+                final Animation anim2 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
+                final Animation anim3 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
+                final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        int value = randomDiceValue();
+                        int res = getResources().getIdentifier("ic_" + value, "drawable", "com.example.android.mobileapplications");
+
+                        if (animation == anim1) {
+                            iv_dice1.setImageResource(res);
+                        } else if (animation == anim2) {
+                            iv_dice2.setImageResource(res);
+                        } else if (animation == anim3) {
+                            iv_dice3.setImageResource(res);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                };
+
+                anim1.setAnimationListener(animationListener);
+                anim2.setAnimationListener(animationListener);
+                anim3.setAnimationListener(animationListener);
+
+                iv_dice1.startAnimation(anim1);
+                iv_dice2.startAnimation(anim2);
+                iv_dice3.startAnimation(anim3);
+            }
+        });
+    }
+
+    public static int randomDiceValue() {
+        return RANDOM.nextInt(6) + 1;
+    }
+}
---
+++ app/src/main/java/com/example/android/mobileapplications/Main.java
@@ -0,0 +1,53 @@
+package com.example.android.mobileapplications;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main extends AppCompatActivity {
+
+    Button btn_start, btn_validate;
+    TextView tv_player;
+    EditText et_player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_player = findViewById(R.id.et_player);
+         btn_start = findViewById(R.id.bn_start);
+         btn_validate = findViewById(R.id.bn_validate);
+
+         tv_player = findViewById(R.id.tv_player);
+
+        validate();
+
+        startGame();
+    }
+
+    private void validate() {
+        btn_validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_player.setVisibility(View.VISIBLE);
+                tv_player.setText("Player 1: " + et_player.getText().toString());
+            }
+        });
+    }
+
+    private void startGame() {
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main.this, Game.class));
+                Toast.makeText(Main.this, "Starting game", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
