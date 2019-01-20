66ebbb943e9b818db108681b7e1b317175d9cc37
kinoodaphne <daphne.kinoo@outlook.be>
2019-01-20 20:15:06 +0100

    v1.4
    Roll dices with numbers (no images)

--- app/src/main/java/com/example/android/mobileapplications/Game.java
+++ app/src/main/java/com/example/android/mobileapplications/Game.java
@@ -1,14 +1,10 @@
 package com.example.android.mobileapplications;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +14,7 @@ public class Game extends AppCompatActivity {
 
     private static final Random RANDOM = new Random();
     Button btn_roll, btn_skip;
-    ImageView iv_dice1, iv_dice2, iv_dice3;
+    TextView tv_dice1, tv_dice2, tv_dice3;
     CheckBox cb_dice1, cb_dice2, cb_dice3;
 
     TextView tv_score;
@@ -33,9 +29,9 @@ public class Game extends AppCompatActivity {
 
         btn_roll = findViewById(R.id.btn_roll);
         btn_skip = findViewById(R.id.btn_skip);
-        iv_dice1 = findViewById(R.id.iv_dice1);
-        iv_dice2 = findViewById(R.id.iv_dice2);
-        iv_dice3 = findViewById(R.id.iv_dice3);
+        tv_dice1 = findViewById(R.id.tv_dice1);
+        tv_dice2 = findViewById(R.id.tv_dice2);
+        tv_dice3 = findViewById(R.id.tv_dice3);
         cb_dice1 = findViewById(R.id.cb_dice1);
         cb_dice2 = findViewById(R.id.cb_dice2);
         cb_dice3 = findViewById(R.id.cb_dice3);
@@ -46,48 +42,15 @@ public class Game extends AppCompatActivity {
     }
 
     private void rollDices() {
+        final Random r = new Random();
         btn_roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 aantalClicks++;
                 if (aantalClicks <= 3) {
-                    final Animation anim1 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
-                    final Animation anim2 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
-                    final Animation anim3 = AnimationUtils.loadAnimation(Game.this, R.anim.shake);
-                    final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            int value = randomDiceValue();
-                            int res = getResources().getIdentifier("ic_" + value, "drawable", "com.example.android.mobileapplications");
-
-                            if (animation == anim1) {
-                                iv_dice1.setImageResource(res);
-                            } else if (animation == anim2) {
-                                iv_dice2.setImageResource(res);
-                            } else if (animation == anim3) {
-                                iv_dice3.setImageResource(res);
-                            }
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-
-                        }
-                    };
-
-
-                    anim1.setAnimationListener(animationListener);
-                    anim2.setAnimationListener(animationListener);
-                    anim3.setAnimationListener(animationListener);
-
-                    iv_dice1.startAnimation(anim1);
-                    iv_dice2.startAnimation(anim2);
-                    iv_dice3.startAnimation(anim3);
-
+                    tv_dice1.setText(Integer.toString(r.nextInt(6)+1));
+                    tv_dice2.setText(Integer.toString(r.nextInt(6)+1));
+                    tv_dice3.setText(Integer.toString(r.nextInt(6)+1));
                 } else {
                     Toast.makeText(Game.this, "Turn over, next player", Toast.LENGTH_SHORT).show();
                 }
@@ -95,9 +58,9 @@ public class Game extends AppCompatActivity {
         });
     }
 
-    public static int randomDiceValue() {
-        return RANDOM.nextInt(6) + 1;
-    }
+//    public static int randomDiceValue() {
+//        return RANDOM.nextInt(6) + 1;
+//    }
 
     private void skipTurn() {
         btn_skip.setOnClickListener(new View.OnClickListener() {
